b0664238eaff0c34c19646b0275bad0e957d1f53
dsalas5 <diego.salas@udem.edu>
2021-12-03 21:37:47 -0600

    cambio en app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(  express.static(__dirname + '/public'));
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log('Servidor funcionando');
 
 });
